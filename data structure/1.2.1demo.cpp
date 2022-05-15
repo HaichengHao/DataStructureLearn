27d6626c7d2cf09e4491e6cd103df2d2c5f38606
hamer <2020104216@qq.com>
2022-05-15 14:03:23 +0800

    1.2.1demo

---
+++ data structure/1.2.1demo.cpp
@@ -0,0 +1,20 @@
+//#include<stdio.h>
+//#include<time.h>
+//time_t start_t, stop_t;
+//double diff_t;
+//
+//
+//void loveyou(int i) {
+//	time(&start_t);
+//	if (i > 1) {
+//		loveyou(i - 1);
+//
+//	}printf("love you %d\n", i);
+//	time(&stop_t);
+//	diff_t = difftime(stop_t, start_t);
+//	printf("%lf\n", diff_t);
+//}
+//void main() {
+//
+//	loveyou(500);
+//}
